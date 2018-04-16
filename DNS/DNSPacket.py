587048953af48f049df6fed756579381879ef5a7
Asya Zaostrovskaya <36674295+asyaZao2403@users.noreply.github.com>
2018-04-16 12:04:14 +0500

    DNS server

---
+++ DNS/DNSPacket.py
@@ -0,0 +1,253 @@
+import bitstring
+
+
+class DNSPacket:
+
+    def __init__(self, header=None, question=None, answer=None, authority=None, additional=None):
+        self.header = header
+        self.question = question
+        self.answer = answer
+        self.authority = authority
+        self.additional = additional
+
+    def to_bytes(self):
+        result = self.header.to_bytes()
+        if self.question is not None:
+            for index in range(len(self.question)):
+                result += self.question[index].to_bytes()
+        if self.answer is not None:
+            for index in range(len(self.answer)):
+                result += self.answer[index].to_bytes()
+        if self.authority is not None:
+            for index in range(len(self.authority)):
+                result += self.authority[index].to_bytes()
+        if self.additional is not None:
+            for index in range(len(self.additional)):
+                result += self.additional[index].to_bytes()
+        return result
+
+
+class Header:
+
+    def __init__(self, id, qr=0, opcode=0, authoritative=0, truncated=0, recursion_desired=0,
+                 recursion_available=0, reply_code=0, questions=0, answer_rrs=0, authority_rrs=0, additional_rrs=0):
+        self.id = id  # Идентификация
+        self.qr = qr  # Тип сообщения: 0 - запрос, 1 - ответ
+        self.opcode = opcode  # Код операции: 0 - стандарт, 1 - инверсныйБ 2 - статус сервера
+        self.aa = authoritative  # Авторитетный ответ
+        self.tc = truncated  # Обрезано
+        self.rd = recursion_desired  # Требуется рукурсия
+        self.ra = recursion_available  # Рекурсия возможна
+        self.rcode = reply_code  # Код возврата: 3 - имени домена не существует
+        self.qdcount = questions  # Количество вопросов
+        self.ancount = answer_rrs  # Количество обычных ответов
+        self.nscount = authority_rrs  # Полномочный источник
+        self.arcount = additional_rrs  # Дополнительная информация
+
+    def to_bytes(self):
+        bits_packet = bitstring.BitArray(length=96)
+        bits_packet[0:16] = bitstring.pack('uint: 16', self.id)
+        bits_packet[16:17] = bitstring.pack('uint: 1', self.qr)
+        bits_packet[17:21] = bitstring.pack('uint: 4', self.opcode)
+        bits_packet[21:22] = bitstring.pack('uint: 1', self.aa)
+        bits_packet[22:23] = bitstring.pack('uint: 1', self.tc)
+        bits_packet[23:24] = bitstring.pack('uint: 1', self.rd)
+        bits_packet[24:25] = bitstring.pack('uint: 1', self.ra)
+        bits_packet[28:32] = bitstring.pack('uint: 4', self.rcode)
+        bits_packet[32:48] = bitstring.pack('uint: 16', self.qdcount)
+        bits_packet[48:64] = bitstring.pack('uint: 16', self.ancount)
+        bits_packet[64:80] = bitstring.pack('uint: 16', self.nscount)
+        bits_packet[80:96] = bitstring.pack('uint: 16', self.arcount)
+        return bits_packet.tobytes()
+
+    def set_id(self, id):
+        self.id = id
+
+    def set_ancount(self, n):
+        self.ancount = n
+
+
+class Question:
+    def __init__(self, qname, qtype, qclass):
+        self.qname = qname  # Доменное имя
+        self.qtype = qtype  # Тип запроса: 1-А, 2-NS, 5-СNAME, 6-SOA, 12-PTR, 28-AAAA
+        self.qclass = qclass  # Класс запроса
+
+    def to_bytes(self):
+        bytes_name = name_to_bytes(self.qname)[0]
+        bits_packet = bitstring.BitArray(length=32)
+        bits_packet[0:16] = bitstring.pack('uint: 16', self.qtype)
+        bits_packet[16:32] = bitstring.pack('uint: 16', self.qclass)
+        return bytes_name + bits_packet.tobytes()
+
+
+class Answer:
+    def __init__(self, aname, atype, aclass, ttl, data_length, address):
+        self.aname = aname  # Доменное имя
+        self.atype = atype  # Тип запроса
+        self.aclass = aclass  # Класс запроса
+        self.ttl = ttl  # Время жизни записи в кеше
+        self.rdlength = data_length  # Размер данных
+        self.rdata = address  # адресс
+
+    def __str__(self):
+        return f'    Name: {self.aname}\n' \
+               f'    Type: ({self.atype})\n' \
+               f'    Class: IN ({self.aclass})\n' \
+               f'    Time to live: {self.ttl}\n' \
+               f'    Data length: {self.rdlength}\n' \
+               f'    Address: {self.rdata}\n'
+
+    def __eq__(self, other):
+        return self.aname == other.aname and self.atype == other.atype \
+               and self.aclass == other.aclass and self.ttl == other.ttl \
+               and self.rdlength == other.rdlength and self.rdata == other.rdata
+
+    def to_bytes(self):
+        bytes_name = name_to_bytes(self.aname)[0]
+        bits_packet = bitstring.BitArray()
+        bits_packet[0:16] = bitstring.pack('uint: 16', self.atype)
+        bits_packet[16:32] = bitstring.pack('uint: 16', self.aclass)
+        bits_packet[32:64] = bitstring.pack('uint: 32', self.ttl)
+        if self.atype == 1:
+            address = address_to_bytes(self.rdata)
+            bits_packet[64:86] = bitstring.pack('uint: 16', self.rdlength)
+        else:
+            address, length = name_to_bytes(self.rdata)
+            bits_packet[64:86] = bitstring.pack('uint: 16', length)
+        return bytes_name + bits_packet.tobytes() + address
+
+
+def get_bit_packet(data):
+    return bitstring.Bits(data)
+
+
+def read_header(bit_packet: bitstring.Bits):
+    id = bit_packet[0:16].uint
+    qr = bit_packet[16:17].uint
+    opcode = bit_packet[17:21].uint
+    aa = bit_packet[21:22].uint
+    tc = bit_packet[22:23].uint
+    rd = bit_packet[23:24].uint
+    ra = bit_packet[24:25].uint
+    rcode = bit_packet[28:32].uint
+    qcount = bit_packet[32:48].uint
+    ancount = bit_packet[48:64].uint
+    nscount = bit_packet[64:80].uint
+    arcount = bit_packet[80:96].uint
+    return Header(id, qr, opcode, aa, tc, rd, ra,
+                  rcode, qcount, ancount, nscount, arcount)
+
+
+def read_questions(bit_packet: bitstring.Bits, header: Header):
+    questions = []
+    start_index = 96
+    for index in range(header.qdcount):
+        question, end_index = read_question(bit_packet, start_index)
+        questions.append(question)
+        start_index = end_index
+    return questions, start_index
+
+
+def read_question(bit_packet: bitstring.Bits, start_index):
+    qname, end_name_index = read_name(bit_packet, start_index, name='')
+    qtype = bit_packet[end_name_index:end_name_index + 16].uint
+    qclass = bit_packet[end_name_index + 16:end_name_index + 32].uint
+    return Question(qname, qtype, qclass), end_name_index + 32
+
+
+def read_answers(bit_packet: bitstring.Bits, header: Header, start_index):
+
+    def get_answers(count, start_index):
+        if count > 0:
+            answers = []
+            for index in range(count):
+                answer, end_index = read_answer(bit_packet, start_index)
+                answers.append(answer)
+                start_index = end_index
+            return answers, start_index
+        else:
+            return [], start_index
+
+    answers_rrs, start_index = get_answers(header.ancount, start_index)
+    authority_rrs, start_index = get_answers(header.nscount, start_index)
+    additional_rrs, start_index = get_answers(header.arcount, start_index)
+
+    return answers_rrs, authority_rrs, additional_rrs
+
+
+def read_answer(bit_packet: bitstring.Bits, start_index):
+    aname, end_name_index = read_name(bit_packet, start_index, name='')
+    atype = bit_packet[end_name_index:end_name_index + 16].uint
+    aclass = bit_packet[end_name_index + 16:end_name_index + 32].uint
+    ttl = bit_packet[end_name_index + 32:end_name_index + 64].uint
+    data_length = bit_packet[end_name_index + 64: end_name_index + 80].uint
+    address, end_index = read_address(bit_packet, end_name_index + 80, data_length, atype)
+    return Answer(aname, atype, aclass, ttl, data_length, address), end_index
+
+
+def read_name(bit_packet: bitstring.Bits, index, name):
+    count_of_char = bit_packet[index:index + 8].uint
+    while count_of_char != 0:
+        if count_of_char >= 192:
+            hoop_place = bit_packet[index + 2:index + 16].uint * 8
+            name = read_name(bit_packet, hoop_place, name)[0]
+            return name, index + 16
+        else:
+            index += 8
+            for i in range(count_of_char):
+                name += bit_packet[index:index + 8].bytes.decode('ASCII')
+                index += 8
+            name += '.'
+        count_of_char = bit_packet[index:index + 8].uint
+    else:
+        return name[:-1], index + 8
+
+
+def read_address(bit_packet: bitstring.Bits, index, data_length, address_type):
+    if address_type == 1:
+        address = ''
+        for i in range(data_length):
+            address += str(bit_packet[index:index + 8].uint)
+            index = index + 8
+            address += '.'
+        return address[:-1], index
+    elif address_type == 2:
+        address, index = read_name(bit_packet, index, name='')
+        return address, index
+
+
+def name_to_bytes(name):
+    bits_name = bitstring.BitArray()
+    name_parts_array = name.split('.')
+    name_length = 0
+    index = 0
+    for name_part in name_parts_array:
+        bits_name[index:index + 8] = bitstring.pack('uint: 8', len(name_part))
+        name_length += len(name_part) + 1
+        index += 8
+        for char in name_part:
+            bits_name[index:index + 8] = bitstring.pack('hex: 8', char.encode('ASCII').hex())
+            index += 8
+    bits_name[index: index + 8] = bitstring.pack('uint: 8', 0)
+    name_length += 1
+    return bits_name.tobytes(), name_length
+
+
+def address_to_bytes(address):
+    bits_address = bitstring.BitArray()
+    address_parts_array = address.split('.')
+    index = 0
+    for address_part in address_parts_array:
+        address = int(address_part)
+        bits_address[index:index + 8] = bitstring.pack('uint: 8', address)
+        index += 8
+    return bits_address.tobytes()
+
+
+def read_dns_packet(data):
+    bit_data = get_bit_packet(data)
+    header = read_header(bit_data)
+    questions, index = read_questions(bit_data, header)
+    answer_rrs, authority_rrs, additional_rrs = read_answers(bit_data, header, index)
+    return DNSPacket(header, questions, answer_rrs, authority_rrs, additional_rrs)
---
+++ DNS/DNSServer.py
@@ -0,0 +1,153 @@
+import pickle
+import socket
+import threading
+import time
+import os
+import sys
+from DNSPacket import *
+
+
+CACHE = dict()
+
+
+def save_cache():
+    global CACHE
+    output = open('cache_data.pkl', 'wb')
+    pickle.dump(CACHE, output, 2)
+    output.close()
+
+
+def build_cache():
+    global CACHE
+    file = 'cache_data.pkl'
+    if os.access(file, os.F_OK):
+        input = open(file, 'rb')
+        CACHE = pickle.load(input)
+        input.close()
+    else:
+        CACHE = dict()
+
+
+def add_records_to_cache(packet: DNSPacket):
+
+    def get_answers(answers):
+        if answers is not None:
+            for ans in answers:
+                if ans.atype in {1, 2}:
+                    add_record(ans)
+
+    def add_record(r):
+        key = (r.aname, r.atype)
+        if key in CACHE:
+            CACHE[key].add(CacheUnit(r, time.time(), r.ttl))
+        else:
+            CACHE[key] = {CacheUnit(r, time.time(), r.ttl)}
+
+    get_answers(packet.answer)
+    get_answers(packet.additional)
+    get_answers(packet.authority)
+    print("CACHE "+str(CACHE))
+
+
+def get_from_cache(key, packet: DNSPacket):
+    data = [(p.rr.rdata, p.rr.rdlength) for p in CACHE[key] if time.time() - p.time <= p.packet_ttl]
+    rdata = [d[0] for d in data]
+    rdl = data[0][1]
+    for data in rdata:
+        packet.answer.append(Answer(key[0], key[1], 1, 300, rdl, data))
+    packet.header.set_ancount(len(rdata))
+    return packet
+
+
+class CacheUnit:
+    def __init__(self, answer: Answer, time, ttl):
+        self.rr = answer  # answer object
+        self.time = time
+        self.packet_ttl = ttl
+
+    def __str__(self):
+        return "data: " + str(self.rr) + "\ntime adding: " \
+               + str(self.time) + "\nttl: " + str(self.packet_ttl)
+
+    def __hash__(self):
+        return hash(self.rr.atype)
+
+    def __eq__(self, other):
+        if type(other) != type(self):
+            return False
+        return self.rr == other.rr
+
+class DNSServer(threading.Thread):
+    def __init__(self, data: DNSPacket, addr, server, sock):
+        threading.Thread.__init__(self)
+        self.client = addr
+        self.port = 53
+        self.data = data
+        self.server = server
+        self.client_sock = sock
+        self.request = None
+
+    def ask_server(self):
+        sock_ask_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock_ask_server.settimeout(1)
+        try:
+            sock_ask_server.sendto(self.request.to_bytes(), (self.server, self.port))
+            response = sock_ask_server.recv(1024)
+            response_packet = read_dns_packet(response)
+            print("\nResponse from SERVER\nFor: " + str(response_packet.answer[0].aname))
+            self.client_sock.sendto(response_packet.to_bytes(), self.client)
+            add_records_to_cache(response_packet)
+        except socket.error:
+            print("Wait... smth wrong")
+
+    def ask_cache(self, key, id):
+        reply = get_from_cache(key, self.request)
+        if reply:
+            print("\nResponse from CACHE\nFor: " + str(key[0]))
+            reply.header.set_id(id)
+            add_records_to_cache(reply)
+            self.client_sock.sendto(reply.to_bytes(), self.client)
+        else:
+            CACHE.__delitem__(key)
+            self.ask_server()
+
+    def start(self):
+        self.request = read_dns_packet(self.data)
+        key = (self.request.question[0].qname, self.request.question[0].qtype)  # имя домена и тип запроса
+        if key in CACHE:
+            self.ask_cache(key, self.request.header.id)
+        elif key[1] in {1, 2}:
+            self.ask_server()
+
+
+def main(arg_server):
+    build_cache()
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        server = arg_server
+        if arg_server == "127.0.0.1":
+            server = "8.8.8.8"
+        client_socket.bind(('127.0.0.1', 53))
+        print("Start dns-server listening {0} port with {1} server".format(53, server))
+        print("Waiting for request...")
+        while True:
+            data, addr = client_socket.recvfrom(1024)
+            DNSServer(data, addr, server, client_socket).start()
+    except KeyboardInterrupt:  # не реагирует
+        client_socket.close()
+        save_cache()
+        print("\nDNS server was stopped\nCache was saved")
+        sys.exit(0)
+    except Exception as ex:
+        client_socket.close()
+        print(ex)
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    try:
+        # main("212.193.163.6")
+        main(sys.argv[1])
+    except IndexError:
+        print("Enter the main server")
+        sys.exit(0)
